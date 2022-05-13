274ae941406720194b8198ecaf8fefcaad65644e
russellgoldenberg <russellgoldenberg@gmail.com>
2022-05-13 15:06:52 -0400

    rm test

--- src/routes/test.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { browser } from "$app/env";
-  import Meta from "$components/Meta.svelte";
-  import App from "$components/App.Test.svelte";
-  if (browser) console.log("svelte-starter version", __VERSION__);
-</script>
-
-<Meta />
-<App />
