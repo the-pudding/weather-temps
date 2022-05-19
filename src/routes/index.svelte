04b7acaee33c807d006b2eea4fa6a6f03f2fc413
russellgoldenberg <russellgoldenberg@gmail.com>
2022-05-19 15:43:42 -0400

    version

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,7 @@
 <script>
-  import { browser } from "$app/env";
   import Meta from "$components/Meta.svelte";
   import App from "$components/App.svelte";
-  if (browser) console.log("svelte-starter version", __VERSION__);
+  import version from "$utils/version.js";
   import { title, description, url, keywords } from "$data/doc.json";
 
   const preloadFont = [
@@ -10,6 +9,8 @@
     "https://pudding.cool/assets/fonts/inter/Inter-SemiBold.woff2",
     "https://pudding.cool/assets/fonts/lyon/LyonDisplay-Regular-Web.woff2"
   ];
+
+  version();
 </script>
 
 <Meta {title} {description} {url} {keywords} {preloadFont} />
