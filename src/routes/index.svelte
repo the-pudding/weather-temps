60bb2faf5cf767a1c5646d07f7a4921b8f05721c
russellgoldenberg <russellgoldenberg@gmail.com>
2022-05-19 11:31:47 -0400

    optimize assets

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,13 @@
   import App from "$components/App.svelte";
   if (browser) console.log("svelte-starter version", __VERSION__);
   import { title, description, url, keywords } from "$data/doc.json";
+
+  const preloadFont = [
+    "https://pudding.cool/assets/fonts/inter/Inter-Regular.woff2",
+    "https://pudding.cool/assets/fonts/inter/Inter-SemiBold.woff2",
+    "https://pudding.cool/assets/fonts/lyon/LyonDisplay-Regular-Web.woff2"
+  ];
 </script>
 
-<Meta {title} {description} {url} {keywords} />
+<Meta {title} {description} {url} {keywords} {preloadFont} />
 <App />
